e1f67a489ca271bd87487eaa3982b6b75cf16e44
UltraTiers <onlycoopy5@gmail.com>
2025-12-18 15:29:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -136,7 +136,8 @@ function setLoggedInUser(user) {
 
   // Load banner
 const player = players.find(p => p.uuid === user.uuid);
-profileBanner.style.backgroundImage = player?.banner ? `url(${player.banner})` : "none";
+profileBanner.style.backgroundImage =
+  `url(${player?.banner || "anime-style-clouds.jpg"})`;
 }
 
 
@@ -586,7 +587,8 @@ return `
 const nitroClass = player.nitro ? "nitro" : "";
 
 modalContent.innerHTML = `
-  <div class="modal-header" style="background-image: ${player.banner ? `url(${player.banner})` : 'none'}">
+  <div class="modal-header"
+     style="background-image: url(${player.banner || 'anime-style-clouds.jpg'})">
     <img class="modal-avatar ${nitroClass}" src="https://render.crafty.gg/3d/bust/${player.uuid}" alt="${player.name} Avatar">
     <div class="modal-name ${nitroClass}">${player.name || "Unknown Player"}</div>
   </div>
@@ -679,7 +681,8 @@ return `
 const nitroClass = player.nitro ? "nitro" : "";
 
 modalContent.innerHTML = `
-<div class="modal-header" style="background-image: ${player.banner ? `url(${player.banner})` : 'none'}">
+  <div class="modal-header"
+     style="background-image: url(${player.banner || 'anime-style-clouds.jpg'})">
     <img class="modal-avatar ${nitroClass}" src="https://render.crafty.gg/3d/bust/${player.uuid}" alt="${player.name} Avatar">
     <div class="modal-name ${nitroClass}">${player.name || "Unknown Player"}</div>
   </div>
--- server.js
+++ server.js
@@ -58,7 +58,7 @@ async function saveOrUpdatePlayer(player) {
         tiers,
         points,
         nitro: nitro || false,
-        banner: banner !== undefined ? banner : existing?.banner || null
+        banner: banner || existing.banner || "anime-style-clouds.jpg"
       })
       .eq("uuid", uuid);
 
@@ -181,7 +181,7 @@ app.get("/players", async (req, res) => {
   tiers: fullTiers,
   points,
   nitro: p.nitro || false,
-  banner: p.banner || null
+  banner: p.banner || "anime-style-clouds.jpg"
 };
     });
 
