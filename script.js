09056b705b5fe162d3ccee28444b8510b42ac725
UltraTiers <onlycoopy5@gmail.com>
2026-01-22 09:02:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -450,6 +450,70 @@ function getPlayerPlacement(player, region = "global") {
   return index >= 0 ? index + 1 : null;
 }
 
+// Show PLAYER modal when clicking on a player
+function showPlayerModal(player) {
+  modalTitle.textContent = "";
+
+  const sortedTiers = sortPlayerTiers(player.tiers, player.retired_modes);
+  const tiersHTML = sortedTiers
+    .map(t => {
+      const tierMatch = t.tier.match(/\d+/);
+      if (!tierMatch) return `<div class="tier empty"></div>`; // fallback for invalid tier
+      const tierNumber = t.tier.match(/\d+/)?.[0];
+      const tierRank = t.tier.startsWith("HT") ? "HT" : "LT";
+
+      return `
+<div class="tier ${player.retired_modes?.includes(t.gamemode) ? "retired" : ""}"
+  data-gamemode="${t.gamemode}"
+  data-tier="${tierNumber}"
+  data-rank="${tierRank}"
+  data-tooltip="${t.gamemode} — ${t.tier}">
+    <img src="gamemodes/${t.gamemode}.png">
+    <span>${tierRank}${tierNumber}</span>
+</div>
+`;
+    }).join("");
+
+  const nitroClass = player.nitro ? "nitro" : "";
+
+  modalContent.innerHTML = `
+  <div class="modal-header"
+     style="background-image: url(${player.banner || 'anime-style-stone.jpg'})">
+    <img class="modal-avatar ${nitroClass}" src="https://render.crafty.gg/3d/bust/${player.uuid}" alt="${player.name} Avatar">
+    <div class="modal-name ${nitroClass}">${player.name || "Unknown Player"}</div>
+  </div>
+
+  <div class="modal-section">
+    <div class="modal-info-row ${nitroClass}">
+      <span class="modal-label">Placement:</span>
+      <span class="modal-value">#${getPlayerPlacement(player)}</span>
+    </div>
+
+    <div class="modal-info-row ${nitroClass}">
+      <span class="modal-label">Region:</span>
+      <span class="modal-value">${player.region || "Unknown"}</span>
+    </div>
+
+    <div class="modal-info-row ${nitroClass}">
+      <span class="modal-label">Rank:</span>
+      <span class="modal-value">${getRankTitle(player.points)}</span>
+    </div>
+
+    <div class="modal-info-row ${nitroClass}">
+      <span class="modal-label">Points:</span>
+      <span class="modal-value">${player.points.toLocaleString()}</span>
+    </div>
+  </div>
+
+  <h3 class="modal-subtitle ${nitroClass}">Tier Progress</h3>
+  <div class="tiers-container">
+    ${tiersHTML}
+  </div>
+`;
+
+  modal.classList.add("show");
+}
+
 // Close PLAYER modal with X button
 closeModalBtn.addEventListener("click", () => {
   modal.classList.remove("show");
