9fa6e39bf438e7ef009ffccc73ab6ee404584c61
UltraTiers <onlycoopy5@gmail.com>
2026-03-02 18:55:12 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -49,20 +49,6 @@ app.post("/brawltiers", async (req, res) => {
   }
 });
 
-import express from "express";
-import cors from "cors";
-import path from "path";
-import fetch from "node-fetch";
-import { createClient } from "@supabase/supabase-js";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(process.cwd())));
-
 // -------------------
 // Supabase setup
 // -------------------
