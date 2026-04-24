3949adc6e4b82783d834903cab2c7ddcc52209bc
UltraTiers <onlycoopy5@gmail.com>
2026-04-25 01:12:32 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SiteShell } from "@/components/site-shell";
-import { fetchPlayers } from "@/lib/api";
-
-export default async function Home() {
-  const players = await fetchPlayers();
-
-  return <SiteShell initialPlayers={players} />;
-}
